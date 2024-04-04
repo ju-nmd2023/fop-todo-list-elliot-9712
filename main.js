fee480d244da8631a66db691aa72f017dca4aa00
Elliot <elliot.2.nilsson@gmail.com>
2024-04-04 14:40:43 +0200

    input added to array and shows on the website

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+const inputArray = [];
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("submitButton").addEventListener("click", function() {
         const inputValue = document.getElementById("list-input").value;
-        console.log(inputValue);
+        
+        inputArray.push(inputValue);
+        
+        document.getElementById("list-input").value = "";
+        document.getElementById("listContent").innerHTML = "";
+        
+        const ul = document.createElement("ul");
+        
+        inputArray.forEach(function(item) {
+            const li = document.createElement("li");
+            li.textContent = item;
+            ul.appendChild(li);
+        });
+        
+        document.getElementById("listContent").appendChild(ul);
+        console.log(inputArray);
     });
 });
-
